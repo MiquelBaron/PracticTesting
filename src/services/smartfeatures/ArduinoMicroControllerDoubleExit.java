14903c40e2ec26fb495e93c76d888e07e730c18a
Miquel Baron <miquelbaron111@gmail.com>
2024-12-29 13:07:34 +0100

    Acabat

--- src/services/smartfeatures/ArduinoMicroControllerDoubleExit.java
+++ src/services/smartfeatures/ArduinoMicroControllerDoubleExit.java
@@ -6,23 +6,31 @@ import exceptions.ProceduralException;
 import java.net.ConnectException;
 
 public class ArduinoMicroControllerDoubleExit implements ArduinoMicroController{
+    private boolean connected;
+    private boolean driving;
+    public ArduinoMicroControllerDoubleExit(){
+        this.connected=false;
+        this.driving=false;
+    }
+
+    //Simulating connection and driving
     @Override
     public void setBTconnection() throws ConnectException {
-
+        connected=true;
     }
 
     @Override
     public void startDriving() throws PMVPhisicalException, ConnectException, ProceduralException {
-
+        driving=true;
     }
 
     @Override
     public void stopDriving() throws PMVPhisicalException, ConnectException, ProceduralException {
-
+        driving=false;
     }
 
     @Override
     public void undoBTconnection() {
-
+        connected=false;
     }
 }
--- src/services/smartfeatures/UnbondedBTSignalDoubleExit.java
+++ src/services/smartfeatures/UnbondedBTSignalDoubleExit.java
@@ -3,8 +3,13 @@ package services.smartfeatures;
 import java.net.ConnectException;
 
 public class UnbondedBTSignalDoubleExit implements UnbondedBTSignal{
+    private boolean connected;
+    public UnbondedBTSignalDoubleExit(){
+        connected=false;
+    }
     @Override
     public void BTbroadcast() throws ConnectException {
-        //Connexió establida
+        connected=true;
     }
-}
+    }
+
